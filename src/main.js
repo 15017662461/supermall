77d8a193a80bf3dc6ced5e47d6e60f76c7c1b4f1
lqh <15017662461@163.com>
2020-08-20 12:51:21 +0800

    day6

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'common/toast/index.js'
+import  FastClick  from  'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 // import 'amfe-flexible'
 
 (function setRem() {
@@ -16,6 +18,12 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 
 Vue.use(toast)
+
+FastClick.attach(document.body)
+
+Vue.use(VueLazyLoad,{
+  loading:require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   router,
